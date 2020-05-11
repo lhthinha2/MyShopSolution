c587001dc0e4cdc4deb57b2bdb6ee26d2da3898c
lhthinha2 <49337610+lhthinha2@users.noreply.github.com>
2020-05-11 21:09:14 +0700

    Add configuration

---
+++ MyShopSolution.Data/Configurations/OrderDetailCofiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyShopSolution.Data.Configurations
+{
+    public class OrderDetailCofiguration : IEntityTypeConfiguration<OrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.HasKey(x => new { x.OrderId, x.ProductId });
+
+            builder.ToTable("OrderDetails");
+
+            builder.HasOne(x => x.Order).WithMany(x => x.OrderDetails).HasForeignKey(x => x.OrderId);
+
+            builder.HasOne(x => x.Product).WithMany(x => x.OrderDetails).HasForeignKey(x => x.ProductId);
+        }
+    }
+}
--- MyShopSolution.Data/EF/MyShopDbContext.cs
+++ MyShopSolution.Data/EF/MyShopDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyShopSolution.Data.Configurations;
 using MyShopSolution.Data.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,15 @@ namespace MyShopSolution.Data.EF
     {
         public MyShopDbContext(DbContextOptions options) : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+
+            //base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<Product> Products { get; set; }
