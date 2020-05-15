05139291b2a68697d49434dc6abf966af704c991
lhthinha2 <49337610+lhthinha2@users.noreply.github.com>
2020-05-15 16:34:34 +0700

    test

--- MyShopSolution.Application/Catalogs/Products/IPublicProductService.cs
+++ MyShopSolution.Application/Catalogs/Products/IPublicProductService.cs
@@ -10,7 +10,5 @@ namespace MyShopSolution.Application.Catalogs.Products
     public interface IPublicProductService
     {
         Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request);
-
-        Task<List<ProductViewModel>> GetAll(string languageId);
     }
 }
